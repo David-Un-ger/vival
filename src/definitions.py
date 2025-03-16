d64125190bed7198f7acd086af395102b6d59067
David Unger <david.unger.de@gmail.com>
2025-03-16 20:50:10 +0100

    fix: create image folder

--- src/definitions.py
+++ src/definitions.py
@@ -3,4 +3,5 @@ from pathlib import Path
 COLOR = "teal.8"  # deprecated
 ROOT_PATH = Path(__file__).parent.parent
 IMAGE_FOLDER = ROOT_PATH / "assets" / "images"
+IMAGE_FOLDER.mkdir(parents=True, exist_ok=True)
 DICTIONARY_TABLE_NAME = "dictionary_v1_2"
